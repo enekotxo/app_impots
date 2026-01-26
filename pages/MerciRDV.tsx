4616a85e8706d3297d4d908ec9803b0c2fca1032
enekotxo <enekotxo@hotmail.com>
2026-01-26 17:25:01 +0100

    Add MerciRDV component for appointment confirmation

---
+++ pages/MerciRDV.tsx
@@ -0,0 +1,8 @@
+export default function MerciRDV() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Merci 🙏</h1>
+      <p>Votre rendez-vous est confirmé. Vous allez recevoir un email avec les instructions</p>
+    </div>
+  );
+}
